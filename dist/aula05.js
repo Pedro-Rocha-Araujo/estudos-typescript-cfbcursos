27cbd0f0f9db46f4b63ce11a9e0a1cfbcb7277fc
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-06-09 19:30:18 -0300

    Aula 5 concluida

---
+++ dist/aula05.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const bandas = ["Metallica", "Black Sabbath", "Avenged Sevenfold"];
+bandas.push("Megadeth");
+bandas.push(2);
+//# sourceMappingURL=aula05.js.map
---
+++ src/aula05.ts
@@ -0,0 +1,4 @@
+const bandas:(string|number)[] = ["Metallica", "Black Sabbath", "Avenged Sevenfold"]
+
+bandas.push("Megadeth")
+bandas.push(2)
