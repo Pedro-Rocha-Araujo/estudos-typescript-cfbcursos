efc370c2ef253af4dd97c16a28b12dba62df4e73
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-06-09 19:15:12 -0300

    Aula 4 concluida

---
+++ dist/aula04.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Pessoa {
+    nome = "";
+    idade = 0;
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+const p1 = new Pessoa("Pedro", 21);
+//# sourceMappingURL=aula04.js.map
---
+++ src/aula04.ts
@@ -0,0 +1,10 @@
+class Pessoa {
+  nome = ""
+  idade = 0
+  constructor(nome:string, idade:number){
+    this.nome = nome
+    this.idade = idade
+  }
+}
+
+const p1 = new Pessoa("Pedro", 21)
