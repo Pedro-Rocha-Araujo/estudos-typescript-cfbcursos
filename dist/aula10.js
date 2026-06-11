35d46e9917a976254228e4b44c22fa4f316dff42
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-06-11 20:10:41 -0300

    Aula 10 sobre unknown undefined e null concluida

---
+++ dist/aula10.js
@@ -0,0 +1,8 @@
+"use strict";
+// Diferença entre Null, Undefined e Unknown
+Object.defineProperty(exports, "__esModule", { value: true });
+let nome;
+console.log("Antes de declarar -> ", nome);
+nome = "Pedro";
+console.log("Depis de declarar -> ", nome);
+//# sourceMappingURL=aula10.js.map
---
+++ src/aula10.ts
@@ -0,0 +1,9 @@
+// Diferença entre Null, Undefined e Unknown
+
+let nome
+
+console.log("Antes de declarar -> ", nome)
+
+nome = "Pedro"
+
+console.log("Depis de declarar -> ", nome)
