fa06ffa991e9a6186081d7d3a71769f023e4a0ca
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-06-11 19:38:14 -0300

    Aula 9 sobre enum concluida

---
+++ dist/aula09.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var dias;
+(function (dias) {
+    dias[dias["segunda"] = 0] = "segunda";
+    dias[dias["terca"] = 1] = "terca";
+    dias[dias["quarta"] = 2] = "quarta";
+    dias[dias["quinta"] = 3] = "quinta";
+    dias[dias["sexta"] = 4] = "sexta";
+    dias[dias["sabado"] = 5] = "sabado";
+    dias[dias["domingo"] = 6] = "domingo";
+})(dias || (dias = {}));
+console.log(dias.segunda);
+console.log(dias[0]);
+//# sourceMappingURL=aula09.js.map
---
+++ src/aula09.ts
@@ -0,0 +1,12 @@
+enum dias {
+  segunda = 0,
+  terca = 1,
+  quarta = 2,
+  quinta = 3,
+  sexta = 4,
+  sabado = 5,
+  domingo = 6
+}
+
+console.log(dias.segunda)
+console.log(dias[0])
