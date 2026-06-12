3c929884d81bbee683f7fff7414dfb2bb4100180
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-06-12 19:04:53 -0300

    Aula 11 sobre Afirmação de tipo e conversão concuida

---
+++ dist/aula11.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let valorQualquer;
+let valorNumber = 1;
+valorQualquer = valorNumber;
+console.log(typeof (valorQualquer));
+//# sourceMappingURL=aula11.js.map
---
+++ src/aula11.ts
@@ -0,0 +1,7 @@
+let valorQualquer
+
+let valorNumber: number = 1
+
+valorQualquer = <number>valorNumber
+
+console.log(typeof(valorQualquer))
