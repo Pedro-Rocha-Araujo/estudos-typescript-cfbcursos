238c9f1e30ff9f96fc35b987a18aa50e6bf4b635
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-06-12 19:25:27 -0300

    Complemento sobre conversão (Testes)

--- dist/aula11.js
+++ dist/aula11.js
@@ -3,5 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let valorQualquer;
 let valorNumber = 1;
 valorQualquer = valorNumber;
-console.log(typeof (valorQualquer));
+// console.log(typeof(valorQualquer))
+let numero = 1;
+let string = "1";
+numero += Number.parseInt(string);
+console.log(numero);
 //# sourceMappingURL=aula11.js.map
--- src/aula11.ts
+++ src/aula11.ts
@@ -4,4 +4,9 @@ let valorNumber: number = 1
 
 valorQualquer = <number>valorNumber
 
-console.log(typeof(valorQualquer))
+// console.log(typeof(valorQualquer))
+
+let numero:number = 1
+let string:string = "1"
+numero += Number.parseInt(string)
+console.log(numero)
