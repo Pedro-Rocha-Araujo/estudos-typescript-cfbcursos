63e69f715ebf1e085f7faf460b29b8b84f7c54d6
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-06-29 19:39:52 -0300

    Aula 36 -> continuação sobre desestruturação

---
+++ dist/aula36.js
@@ -0,0 +1,10 @@
+// Aula 2 sobre desestruturação
+let [n1, n2 = null, ...n3] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(n1, n2, n3);
+const frase = "Meu nome é Pedro";
+let [...corte] = frase.split(" ");
+console.log(corte);
+const frase2 = "Olá! Como vai?";
+console.log(frase2.split(""));
+export {};
+//# sourceMappingURL=aula36.js.map
